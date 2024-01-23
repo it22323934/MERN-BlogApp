35609a7215e69f652fcbdad9f83f31d266a2243a
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-01-23 10:31:17 +0530

    Created the pages and added the routes-Asiri Jayawardena

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Project from "./pages/Project";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
-  return <h1 className="text-3xl">App</h1>;
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/projects" element={<Project/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+
+  
 }
