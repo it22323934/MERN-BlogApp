554a6707d8e3a461bb7aa40c58b00bff72477c5a
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-01-23 12:05:59 +0530

    MONGO DB connected-Asiri

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>console.log('DB Connected'))
+.catch(err=>console.log(err));
+
 
 const app=express();
 
