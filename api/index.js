2c8bb9a00cbc79b5e056d71119d7b35554794a9f
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-01-23 12:12:52 +0530

    Creatd user model-Asiri

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URL)
-.then(()=>console.log('DB Connected'))
-.catch(err=>console.log(err));
 
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
-const app=express();
+const app = express();
 
-
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
