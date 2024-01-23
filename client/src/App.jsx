66d9c06f3eccdee87a7f178f9c733e9f5e8a9e08
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-01-23 16:38:39 +0530

    Created Footer component-Asiri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp";
 import Project from "./pages/Project";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return(
@@ -20,6 +21,7 @@ export default function App() {
       <Route path="/projects" element={<Project/>} />
       <Route path="/dashboard" element={<Dashboard/>} />
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 
