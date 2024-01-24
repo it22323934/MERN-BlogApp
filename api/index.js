eddf900e9194ffff157ea43296a0bb427ba15ef8
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-01-24 09:46:43 +0530

    added Update api route-Asiri

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -11,6 +12,7 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
