2d6548e44d18d3b2e82c2cf78f45994142986203
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-02-02 20:46:08 +0530

    Add pre installation to render-Asiri

--- api/index.js
+++ api/index.js
@@ -6,12 +6,16 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
+
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(cookieParser());
@@ -25,6 +29,12 @@ app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+});
 
 app.use((err,req,res,next)=>{
   const statusCode=err.statusCode || 500;
